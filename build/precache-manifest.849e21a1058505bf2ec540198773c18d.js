51b7862ca9d3662956d204268f9fd43a834ff9c2
HOWARD1021 <ms04626730@gmail.com>
2020-04-09 21:34:00 +0800

    'Todo_listv1'

---
+++ build/precache-manifest.849e21a1058505bf2ec540198773c18d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2428f3eda7c93f0da5dc27a3fda96cda",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5379a62488da9252a360",
+    "url": "./static/css/main.c5382d94.chunk.css"
+  },
+  {
+    "revision": "b7069e5432872cf5669f",
+    "url": "./static/js/2.f2f053b1.chunk.js"
+  },
+  {
+    "revision": "4171527e1b967fb4da659f784354c39f",
+    "url": "./static/js/2.f2f053b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5379a62488da9252a360",
+    "url": "./static/js/main.4d624dbd.chunk.js"
+  },
+  {
+    "revision": "25cfd55d2bed6e5f42f2",
+    "url": "./static/js/runtime-main.090e3661.js"
+  },
+  {
+    "revision": "2a9a5a4b6cd37bcdca69993fd3ba65b8",
+    "url": "./static/media/road.2a9a5a4b.jpg"
+  }
+]);
